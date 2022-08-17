ba31e2b0fda27071ecae31f407ef6ec73cde95b6
Daisy Simiyu <d.daisysimiyu@gmail.com>
2022-08-17 16:47:46 +0300

    commit tree cf059c08a37abc224b3907636a10fb11cd879aa8

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+  end
+  
+  def batch_badge_creator nameArray
+    nameArray.map { |name| "Hello, my name is #{name}." }
+  end
+  
+  def assign_rooms nameArray
+    nameArray.map { |name| "Hello, #{name}! You'll be assigned to room #{(nameArray.find_index name) + 1}!" }
+  end
+  
+  def printer nameArray
+    badges = batch_badge_creator(nameArray)
+    badges.each { |badge| puts badge }
+  
+    rooms = assign_rooms(nameArray)
+    rooms.each { |room| puts room }
+  end
